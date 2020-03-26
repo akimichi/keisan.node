b0bfcc684c5ab32d50819a8ae08faf32201e0d24
Akimichi Tatsukawa <akimichi.tatsukawa@gmail.com>
2020-03-27 07:18:18 +0900

    Syntax.appのテストを追加

--- test/lib/lispy.test.js
+++ test/lib/lispy.test.js
@@ -208,6 +208,104 @@ describe("Lispyをテストする",() => {
   describe("Lispy.Syntaxをテストする",() => {
     const Syntax = require("../../lib/lispy").Syntax;
 
+    describe("appをテストする",() => {
+      describe("単一引数のappの場合",() => {
+        it("(succ 1 は parse error", function(done) {
+          Maybe.match(Syntax.app()("(succ 1"), {
+            just: (result) => {
+              console.log(message)
+              expect().to.fail()
+            },
+            nothing: (message) => {
+              expect(true).to.eql(true)
+              done();
+            }
+          });
+        })
+        it("(succ 1) はapp式である", function(done) {
+          Maybe.match(Syntax.app()("(succ 1)"), {
+            just: (result) => {
+              Exp.match(result.value, {
+                app: (value) => {
+                  expect(true).to.eql(true)
+                  done();
+                }
+              })
+            },
+            nothing: (message) => {
+              expect(true).to.eql(true)
+              done();
+            }
+          });
+        })
+        it("(succ (succ 1)) はapp式である", function(done) {
+          Maybe.match(Syntax.app()("(succ (succ 1))"), {
+            just: (result) => {
+              Exp.match(result.value, {
+                app: (value) => {
+                  expect(true).to.eql(true)
+                  done();
+                }
+              })
+            },
+            nothing: (message) => {
+              expect(true).to.eql(true)
+              done();
+            }
+          });
+        })
+        it("(succ (succ (succ 1))) はapp式である", function(done) {
+          Maybe.match(Syntax.app()("(succ (succ (succ 1)))"), {
+            just: (result) => {
+              Exp.match(result.value, {
+                app: (value) => {
+                  expect(true).to.eql(true)
+                  done();
+                }
+              })
+            },
+            nothing: (message) => {
+              expect(true).to.eql(true)
+              done();
+            }
+          });
+        })
+      })
+    });
+    describe("複数引数のappの場合",() => {
+      it("(add 1 2) はapp式である", function(done) {
+        Maybe.match(Syntax.app()("(add 1 2)"), {
+          just: (result) => {
+            Exp.match(result.value, {
+              app: (value) => {
+                expect(true).to.eql(true)
+                done();
+              }
+            })
+          },
+          nothing: (message) => {
+            expect(true).to.eql(true)
+            done();
+          }
+        });
+      })
+      it("(add 1 (add 2 3)) はapp式である", function(done) {
+        Maybe.match(Syntax.app()("(add 1 (add 2 3))"), {
+          just: (result) => {
+            Exp.match(result.value, {
+              app: (value) => {
+                expect(true).to.eql(true)
+                done();
+              }
+            })
+          },
+          nothing: (message) => {
+            expect(true).to.eql(true)
+            done();
+          }
+        });
+      })
+    });
     describe("specialをテストする",() => {
       describe("setをテストする",() => {
         it("{set x 1}はset式である", function(done) {
